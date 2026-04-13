2b661b611e93507ed4e258146ceb739fdad5eb4c
Lucvoice12 <boeluka525@gmail.com>
2026-04-13 09:45:34 +0300

    Create set.js

---
+++ config/set.js
@@ -0,0 +1,46 @@
+module.exports = {
+    // ================= BOT INFO =================
+    BOT_NAME: "𝐋𝐔𝐂𝐕𝐎𝐈𝐂𝐄-𝐗𝐌𝐃",
+    OWNER_NAME: "Luka IT",
+    OWNER_NUMBER: "255768619068",
+
+    // ================= PREFIX =================
+    PREFIX: ".",
+
+    // ================= MODE =================
+    MODE: "public", // public / private
+
+    // ================= SESSION =================
+    SESSION: "Zokou-MD-WHATSAPP-BOT;;;=>",
+
+    // ================= FEATURES =================
+    AUTO_READ_STATUS: "yes",
+    AUTO_REACT_STATUS: "yes",
+    AUTO_DOWNLOAD_STATUS: "no",
+
+    ANTI_DELETE_MESSAGE: "yes",
+    ADM: "yes",
+
+    PM_PERMIT: "yes",
+
+    ETAT: 1, // 1=online, 2=typing, 3=recording
+
+    DP: "yes",
+
+    URL: "https://github.com/lucvoice/LUCVOICE-XMD",
+
+    // ================= SECURITY =================
+    SUDO: [
+        "255768619068"
+    ],
+
+    // ================= STICKER INFO =================
+    PACK_NAME: "𝐋𝐔𝐂𝐕𝐎𝐈𝐂𝐄",
+    AUTHOR: "Luka IT",
+
+    // ================= LINKS =================
+    SUPPORT_GROUP: "https://chat.whatsapp.com/xxxx",
+
+    // ================= SYSTEM =================
+    VERSION: "1.0.0"
+};
